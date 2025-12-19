e87230530e6ac12e8b473e9176f459b372e99b7c
Mrtea-hub <cy_mert@hotmail.com>
2025-12-19 23:19:20 +0300

    Model_Improvement

--- src/models/model.py
+++ src/models/model.py
@@ -1,28 +1,30 @@
 import torch
 import torch.nn as nn
 from torchvision import models
-from src.utils import config 
+from src.utils import config
 
-
-num_classes = config.NUM_CLASSES
 
 class ChestXRayResNet(nn.Module):
-    def __init__(self, num_classes=14): 
+    def __init__(self, num_classes=14):
         super(ChestXRayResNet, self).__init__()
-        
+
         # ResNet50'yi önceden eğitilmiş ağırlıklarla yükle
         self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-        
-        
-        # Son katmanın (fc) giriş özellik sayısını al (ResNet-50 için 2048'dir)
-        num_ftrs = self.model.fc.in_features 
-        
-        # Son katmanı (fc) istenen sınıf sayısına (14) uygun yeni bir nn.Linear katmanıyla değiştir
-        # Bu, transfer öğrenimi (fine-tuning) için modelin uyarlanmasıdır.
-        self.model.fc = nn.Linear(num_ftrs, num_classes)
-        
-    def forward(self, x):
-        # Standart ileri besleme (forward) akışı
-        # Girdi (x), ResNet modelinden geçirilir.
-        return self.model(x)
 
+        # --- A. Dondurma (Freezing) İşlemi ---
+        # Layer4 ve FC hariç her şeyi dondur (Kapasite kısıtlama)
+        for name, param in self.model.named_parameters():
+            if "layer4" not in name and "fc" not in name:
+                param.requires_grad = False
+
+        num_ftrs = self.model.fc.in_features
+
+        # --- B. Dropout Ekleme ---
+        # %50 Dropout ekleyerek modelin ezber yapmasını (overfitting) engelliyoruz
+        self.model.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_ftrs, num_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
